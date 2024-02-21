758f85b084778ca80ce0cb15ac72451559d20ebc
mohamed abd elkhalk <01147018091.m@gmial.com>
2024-02-21 22:17:33 +0200

    add netlify plugin and fix route eror

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
+import netlify from "vite-plugin-netlify";
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), netlify()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
