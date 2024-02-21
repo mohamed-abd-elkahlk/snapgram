eb07b9893d7d6622918b6a9ee3179b6fe8f3fa05
mohamed abd elkhalk <01147018091.m@gmial.com>
2024-02-21 22:23:41 +0200

    fix redurct error

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import netlify from "vite-plugin-netlify";
 export default defineConfig({
-  plugins: [react(), netlify()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
